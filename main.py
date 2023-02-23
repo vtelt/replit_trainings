2e672a3775b014cabdd005027ce374175d7f53d9
Victoria Teltova <103757818+vtelt@users.noreply.github.com>
2023-02-23 11:55:06 +0000

    added new files

---
+++ main.py
@@ -0,0 +1,22 @@
+light_is_on = True
+mode_on = False
+
+if light_is_on:
+    print("The light is ON")
+else:
+  print("We're in the dark")
+
+weight = 80.4
+
+if weight < 85:
+  print("You are under weight")
+
+if weight == 85:
+  print("You are under weight")
+
+age = 34
+
+if age >= 18:
+  print("Adult")
+else:
+  print("Child")
