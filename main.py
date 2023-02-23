019710fc900ded7357cb9e925038d4d11424554b
ViktoriiaTel <>
2023-02-23 11:31:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+wallet = 41
+
+print(wallet)
+
+wallet = 32
+
+print(wallet)
+
+day = 3
+month = 'October'
+day_of_week = 'Sunday'
+temp = -1 
+
+weight = 180.48
+
+print(3 + 5)
+
+print(day + 3)
+
+print(weight * 2)
+
+shirt = "blue"
+print(shirt)
+
+store = "Nick's pizza is the \"best\" pizza"
+
+print(store)
+
+print(f"Today is {day_of_week} {month} {day} and it's {temp} degrees")
+
