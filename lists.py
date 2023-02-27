0a9b4b4e39f4d68185aeadc4862446cafbe4ee6c
Victoria Teltova <103757818+vtelt@users.noreply.github.com>
2023-02-27 14:45:20 +0000

    Added lists training

---
+++ lists.py
@@ -0,0 +1,25 @@
+fav_movies = ['Dune', 'Star wars', 'Titanic']
+
+print(fav_movies[1])
+
+print(len(fav_movies))
+
+fav_movies.append("Iron man")
+
+print(len(fav_movies))
+
+print(fav_movies)
+
+fav_movies.insert(1, "Batman")
+
+print(fav_movies)
+
+del(fav_movies[2])
+
+print(fav_movies)
+
+del(fav_movies[0])
+del(fav_movies[0])
+del(fav_movies[0])
+
+print(fav_movies)
