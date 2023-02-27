3d5ff01bd69b22122e431c8a81136910cabc5acb
Victoria Teltova <103757818+vtelt@users.noreply.github.com>
2023-02-27 15:09:26 +0000

    Added loops_dictionaries.py

---
+++ loops_dictionaries.py
@@ -0,0 +1,25 @@
+for number in range(10):
+  print(number)
+
+fav_movies = ['Dune', 'Star wars', 'Titanic']
+
+for movie in fav_movies:
+  print(movie)
+
+for number in range(40):
+  print(number)
+
+#dictionaries
+cats = {"Jane":6, "Tom":4, "Nora":8}
+
+cats["Wilson"] = 1
+print(cats)
+print(cats["Tom"])
+
+del(cats["Tom"])
+
+print(cats)
+
+len(cats)
+
+ints_bools = {5:True, 6:False, 8:False}
